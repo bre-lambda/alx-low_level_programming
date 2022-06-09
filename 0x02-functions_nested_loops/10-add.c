5c52f304213e6c1b1b5d25d11294b8054d2fe0d2
birhanu eniyew <bre.lambda@gmail.com>
2022-06-09 01:13:34 -0700

    q 10 final

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ * and returns the result.
+ * @x: integer 1
+ * @y: integer 2
+ * Return: zero
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - method
+ * Return: zero
+ */
+int main(void)
+{
+	int k;
+
+	k = add(89, 9);
+	printf("%d\n", k);
+	return (0);
+}
