e6dfa86cb113de1050d899acb15626a8fae3a3c2
birhanu eniyew <bre.lambda@gmail.com>
2022-06-09 05:03:42 -0700

    q 6 final

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * main - method
+ * Return: zero
+ */
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: input number
+ * Return: zero
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
