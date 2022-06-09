cebd3a384f131632ae0571a3357b21ffb52acfb6
birhanu eniyew <bre.lambda@gmail.com>
2022-06-09 13:34:12 -0700

    final

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+void print_integer(int m);
+
+/**
+ * print_number - print an integer
+ * @n: number
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_integer(n * -1);
+	}
+	else
+		print_integer(n);
+}
+/**
+ * print_integer - printing numbers
+ * @m: input
+ * Return: nothing
+ */
+void print_integer(int m)
+{
+	int i = 1000000000;
+
+	for (; i >= 1; i /= 10)
+		if (m / i != 0)
+		{
+		_putchar((m / i) % 10 + '0');
+		}
+}
