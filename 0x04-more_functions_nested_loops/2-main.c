5af3f64fb6b68c4a8f51a9a069e975562af26e8b
birhanu eniyew <bre.lambda@gmail.com>
2022-06-09 03:31:29 -0700

    q 2 final

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - method
+ * Return: zero
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplay to intigeres
+ * @x: input 1
+ * @y: input 2
+ * Return: zero
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
