9051ae7eeb818b6f478fedbc0ce118e35d43bcad
birhanu eniyew <bre.lambda@gmail.com>
2022-06-16 09:36:39 -0700

    advanced hotfix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,17 @@
-#include "main.c"
+#include "main.h"
+
 
 /**
- * rot13 - encode string method
- *
+ * rot13 - encode sthetring with rot13
  * @src: string to encode
- * Return: string
+ * Description: uses the rot13 to encode a string
+ * Return: Strring Src
  */
 
 char *rot13(char *src)
 {
 	char c[] = {"NOPQRSTUVWXYZABCDEFGHIJKLM[\\]^_`nopqrstuvwxyzabcdefghijklm"};
+
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,30 @@
 #include "main.h"
 
+#define ROT13IN  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define ROT13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - prints the integer
+ * @n:  the integer to be printed
+ * Description:takes inputs and prints an integer
+ * Return: Nothing
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	int i, p;
 
 	if (n < 0)
-	{
 		_putchar('-');
-		num = -num;
+	for (p = 0, i = 1000000000; i > 0; i /= 10)
+	{
+		int d = (n / i) % 10;
+
+		d = d < 0 ? -d : d;
+
+		if (d || p || i == 1)
+		{
+			_putchar(d + '0');
+			p++;
+		}
 	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
 }
